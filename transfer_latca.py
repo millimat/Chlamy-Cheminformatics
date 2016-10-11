312d3a00c6160fa41188abb0256b2e5e121b2006
Matt Millican <millimat@stanford.edu>
2016-10-11 01:22:42 -0700

    Put all LATCA files in archive latca_all.zip; retransferred unvaildated, validated, and strong purchasable drugs from plates 1-45 to folders lupt45, lvpt45, and lspt45 respectively

--- transfer_latca.py
+++ transfer_latca.py
@@ -1,16 +1,26 @@
+from sys import argv
 from os import listdir, mkdir
 from os.path import isfile, join
 from shutil import copyfile
 
+sourcedir = "latca_all/"
+
 def main():
-    file_of_usables = open("data/strong_purchasable_thru_45.txt")
+    if(len(argv) != 3):
+        print("usage: python {0} [LATCA listing] [target dir]".format(argv[0]))
+        return
+
+    usables_path = argv[1]
+    target = argv[2]
+    
+    file_of_usables = open(usables_path)
     usable_latids = [x.strip() for x in file_of_usables.readlines()]
     file_of_usables.close()
 
-    all_sdfs = [obj for obj in listdir("latca/") if isfile(join("latca/", obj))]
+    all_sdfs = [obj for obj in listdir(sourcedir) if isfile(join(sourcedir, obj))]
     
     try:
-        mkdir("latca_strong_purchasable_thru_45/")
+        mkdir(target)
     except OSError:
         pass
 
@@ -18,7 +28,7 @@ def main():
         
     for sdf_name in all_sdfs:
         if sdf_name[:-4] in usable_latids: # [:-4] strips the ".sdf"
-            copyfile(join("latca/", sdf_name), join("latca_strong_purchasable_thru_45/", sdf_name))
+            copyfile(join(sourcedir, sdf_name), join(target, sdf_name))
             found.append(sdf_name[:-4])
             print("Transferred {0}".format(sdf_name))
 
